988b420e37d54dc2efe9b90f91313a54fa183e97
fillipeags <fillipe484@gmail.com>
2021-03-28 13:00:33 -0300

    sass style for repository list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { Counter } from './components/Counter';
 import { RepositoryLIst } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
 
   return (
-    <>
-    
+    <>  
       <RepositoryLIst />
-      <Counter/>
-    
     </>
   )
 }
