456166294fdf5c40e5c62428f078e2bf104dae08
fillipeags <fillipe484@gmail.com>
2021-03-28 13:24:45 -0300

    list repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,28 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform"
-}
-
 export function RepositoryLIst(){
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/fillipeags/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, []);
+
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+        })}
       </ul>
     </section>
   );
 }
+
+
