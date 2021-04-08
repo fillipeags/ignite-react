6743ff697d9704d534e5088fb72fc1f82d5b6cf5
Fillipe Augusto <fillipe484@gmail.com>
2021-04-08 20:52:29 -0300

    list transactions and seeds

--- cap-2/dtmoney/src/index.tsx
+++ cap-2/dtmoney/src/index.tsx
@@ -8,6 +8,30 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+
+        {
+          id: 2,
+          title: 'Alugel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 110,
+          createdAt: new Date('2021-02-14 10:00:00')
+        }
+      ],
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
