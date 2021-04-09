8e1fe27e4fbc77c01dc742caef78034b04bc665e
Fillipe Augusto <fillipe484@gmail.com>
2021-04-09 20:04:38 -0300

    creating hook

--- cap-2/dtmoney/src/components/NewTransactionModal/index.tsx
+++ cap-2/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,16 +3,15 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { FormEvent, useState, useContext} from 'react';
-import { TransactionsContext } from '../../TransactionsContext';
-
+import { FormEvent, useState} from 'react';
+import { useTransactions } from '../../hooks/useTransactions';
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
-  const {createTransaction} = useContext(TransactionsContext)
+  const {createTransaction} = useTransactions();
 
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
